da0735b1a2cf9bf57198dfa9447117c960aeb3f2
张晋荣 <1052618753@qq.com>
2022-09-18 09:06:27 +0800

    a
    a

--- main.py
+++ main.py
@@ -56,6 +56,11 @@ def get_random_color():
 
 client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
+user_id =  'opet-6VCWWvyzsfu_WwEtERBEaoo'
+wea, temperature = get_weather()
+data = {"weather":{"value":wea},"temperature":{"value":datetime.now().strftime('%H')},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+res = wm.send_template(user_id, template_id, data)
+
 if(datetime.now().strftime('%H')=='9'):
   user_id =  'opet-6VCWWvyzsfu_WwEtERBEaoo'
   wea, temperature = get_weather()
