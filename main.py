5f624c14d47b8d5ce9e62ee276236ef9eec25763
张晋荣 <1052618753@qq.com>
2022-09-18 09:27:53 +0800

    l

--- main.py
+++ main.py
@@ -55,9 +55,10 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 
 while 1:
-  user_id =  'opet-6VCWWvyzsfu_WwEtERBEaoo'
-  wea, temperature = get_weather()
-  data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-  res = wm.send_template(user_id, template_id, data)
-  time.sleep(60)
+  if(datetime.now().strftime('%H')=='01'):
+    user_id =  'opet-6VCWWvyzsfu_WwEtERBEaoo'
+    wea, temperature = get_weather()
+    data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+    res = wm.send_template(user_id, template_id, data)
+    time.sleep(60)
   
