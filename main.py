21c05e51c0c93714f955d3337212f3c1b101dd6d
张晋荣 <1052618753@qq.com>
2022-09-18 09:20:01 +0800

    g
    g

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ for i in range(2):
   wea, temperature = get_weather()
   data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
   res = wm.send_template(user_id, template_id, data)
-  time.sleep(60)
+  
