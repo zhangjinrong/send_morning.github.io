2a45e8775cfdf593420eb32f3aa140c3b79e547c
张晋荣 <1052618753@qq.com>
2022-09-18 08:32:41 +0800

    e
    e

--- main.py
+++ main.py
@@ -5,18 +5,25 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
+import time
 
 today = datetime.now()
+'''
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
-
 user_id = os.environ["USER_ID"]
-
 template_id = os.environ["TEMPLATE_ID"]
+'''
+start_date = '2022-03-19'
+city = '北京'
+birthday = '04-06'
+app_id = 'wx4abfbd3e393507e7'
+app_secret = '037be9a91ee6c7203536049dbc73df10'
+template_id = 'zxO6WSWCw68ZjoefFWTDvnYhOfsf08-2ZeBZzyLlpSI'
+user_id =  'opet-6VmFGa3nWfqo2dgOeSljk1w'
 
 
 def get_weather():
@@ -44,14 +51,19 @@ def get_words():
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
-
+#print(datetime.now().strftime('%H'))
 client = WeChatClient(app_id, app_secret)
-
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
-user_id = os.environ["USER_ID"]
-res = wm.send_template(user_id, template_id, data)
-print(res)
+while 1:
+    if(datetime.now().strftime('%H')=='7'):
+        user_id =  'opet-6VmFGa3nWfqo2dgOeSljk1w'
+        wea, temperature = get_weather()
+        data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+        res = wm.send_template(user_id, template_id, data)
+        user_id =  'opet-6VCWWvyzsfu_WwEtERBEaoo'
+        res = wm.send_template(user_id, template_id, data)
+        time.sleep(60*61)
+    else if(datetime.now().strftime('%H')=='6'):
+        time.sleep(60*10)
+    else:
+        time.sleep(60*50)
