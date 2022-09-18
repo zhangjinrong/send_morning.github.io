332c7a8b23ed52b5a86a20da40d2980b0cf9e0a4
张晋荣 <1052618753@qq.com>
2022-09-18 09:39:00 +0800

    c

--- main.py
+++ main.py
@@ -8,22 +8,18 @@ import random
 import time
 
 today = datetime.now()
-'''
+
+
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
-user_id = os.environ["USER_ID"]
+user_id1 = os.environ["USER_ID"]
+user_id2 = os.environ["USER_ID2"]
 template_id = os.environ["TEMPLATE_ID"]
-'''
-start_date = '2022-03-19'
-city = '北京'
-birthday = '04-06'
-app_id = 'wx4abfbd3e393507e7'
-app_secret = '037be9a91ee6c7203536049dbc73df10'
-template_id = 'zxO6WSWCw68ZjoefFWTDvnYhOfsf08-2ZeBZzyLlpSI'
-user_id =  'opet-6VmFGa3nWfqo2dgOeSljk1w'
+
+
 
 
 def get_weather():
@@ -56,11 +52,11 @@ wm = WeChatMessage(client)
 
 while 1:
   if(datetime.now().strftime('%H')=='23'):
-    user_id =  'opet-6VCWWvyzsfu_WwEtERBEaoo'
+    user_id =  user_id1
     wea, temperature = get_weather()
     data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
     res = wm.send_template(user_id, template_id, data)
-    user_id =  'opet-6VmFGa3nWfqo2dgOeSljk1w'
+    user_id =  user_id2
     res = wm.send_template(user_id, template_id, data)
     time.sleep(60*61)
   elif(datetime.now().strftime('%H')=='22'):
